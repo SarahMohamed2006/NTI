11169dc8b4462658fa07946fecc5fcc632a910a4
Sarah Mohamed <saramohamedw2006@gmail.com>
2026-09-06 20:08:39 +0300

    Add files via upload

--- MCAL/ISR/ISR.h
+++ MCAL/ISR/ISR.h
@@ -1,1 +1,20 @@
+/*
+ * ISR.h
+ *
+ * Created: 9/6/2026 7:58:05 PM
+ *  Author: Missa
+ */ 
 
+
+#ifndef ISR_H_
+#define ISR_H_
+
+
+#include "../SERVICE/STD_TYPES.h"
+
+/* Function Prototype */
+void initExternalInterrupts(void);
+
+
+
+#endif /* ISR_H_ */
