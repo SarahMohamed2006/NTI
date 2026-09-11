1d3e6285b0299139f4016b92f962175c83c7f84f
Sarah Mohamed <saramohamedw2006@gmail.com>
2026-09-11 20:41:51 +0300

    Update ADC.h

--- MCAL/ADC/ADC.h
+++ MCAL/ADC/ADC.h
@@ -1,1 +1,22 @@
 
+/*
+ * ADC.h
+ *
+ * Created: 9/11/2026 8:29:54 PM
+ *  Author: Sarah
+ */ 
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+#define ADMUX_Reg 	*((volatile u8*)0x27) 
+#define ADCSRA_Reg 	*((volatile u8*)0x26) 	
+#define ADCH_Reg 	*((volatile u8*)0x25) 		
+#define ADCL_Reg 	*((volatile u8*)0x24) 		
+
+
+extern void ADC_init (void); // ADC Initialization 
+extern u16 ADC_read (u8  channel_id); 
+
+#endif /* ADC_H_ */
